fc42431bdeb96794b5f9c3162fbb8471333088c4
flavia-kihahu <kihahuflavia@gmail.com>
2026-08-05 05:24:21 +0300

    added ToggleStatus and DeleteTask in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,53 @@ import OrderList from './components/Order.list';
 import './App.css';
 
 export default function App() {
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    name: 'Jane Doe',
+    title: 'Chocolate Cake',
+    quantity: 2,
+    status: 'Pending'
+  },
+  { 
+    id: 2,
+    name: 'John Smith',
+    title: 'Vanilla Cupcake',
+    quantity: 6,
+    status: 'Completed'
+  }]);
 
-const [tasks, setTasks] = useState([{
-  id: 1,
-  name: 'Jane Doe',
-  title: 'Chocolate Cake',
-  quantity: 2,
-  status: 'Pending'
-},
-{ 
-  id: 2,
-  name: 'John Smith',
-  title: 'Vanilla Cupcake',
-  quantity: 6,
-  status: 'Completed'
-}]);
+  const handleAddTask = (newTask) => {
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id : Date.now(),
+        ...newTask
+      }
+    ]);
+  };
 
-const handleAddTask = (newTask) => {
-  setTasks((prevTasks) => [...prevTasks, {id : Date.now(), ...newTask}]);
-}}
+  const handleDeleteTask = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.filter((task) => task.id !== id)
+    );
+  };
 
-return (
-    <>
-      <div className= "App">
-        <h1>Bakery Order Tracker</h1>
-        <Form onAddTask={handleAddTask} />
-        <OrderList tasks={tasks} />
-      </div>
-    </>
-)
+  const handleToggleStatus = (id) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, status: task.status === 'Pending' ? 'Completed' : 'Pending' } : task
+      )
+    );
+  };
+
+  return (
+      <>
+        <div className= "App">
+          <h1>Bakery Order Tracker</h1>
+          <Form onAddTask={handleAddTask} />
+          <OrderList tasks={tasks} onDelete={handleDeleteTask} onToggleStatus={handleToggleStatus} />
+        </div>
+      </>
+  )
+}
+
